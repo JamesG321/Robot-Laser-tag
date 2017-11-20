6533563314f222dd3a2de9ab29e8e6e6e0370f19
James <jamesg22@uw.edu>
2017-11-20 03:29:24 -0800

    added comments

--- Joy/Signals.h
+++ Joy/Signals.h
@@ -1,3 +1,11 @@
+//Robot laser tag game
+//James Guo
+//Autmn 2016
+//Signals.h
+
+//defines ascii sequence for different commands between the controller and the robot.
+//e.g if the controller says move forward, it sends string "11" to the robot via bluetooth communication protocol.
+
 #define FORWARD1 11
 #define FORWARD3 12
 
